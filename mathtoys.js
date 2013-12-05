e8d358e105430c752526f90074cff9e26c7bd55e
Cris Perdue <cris@perdues.com>
2013-12-04 17:10:26 -0800

    Initial version of Mathtoys web site, just code unique to that site.

---
+++ mathtoys.js
@@ -0,0 +1,11 @@
+// Copyright 2011, 2012, 2013 Crispin Perdue.  All rights reserved.
+
+// Boilerplate text for the left navigation area.
+Toy.navText = [
+  '<a class=nav href="index.html">',
+  'Home page</a>',
+  '<a href="simplify.html">Simplification</a>',
+  '<a href="equations.html">Solving equations</a>',
+  '</ul>'
+].join('\n');
+
