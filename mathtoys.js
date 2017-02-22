d4677bf0eeb84c3f9aba5d247d31316fecb902aa
Cris Perdue <cris@perdues.com>
2017-02-21 18:41:38 -0800

    Fix typo affecting left nav.

--- mathtoys.js
+++ mathtoys.js
@@ -6,7 +6,7 @@ Toy.navText = [
   'Home page</a>',
   '<a href="simplify.html">Simplification</a>',
   '<a href="equations.html">Solving equations</a>',
-  '<a href="http://github.com/crisperdue/mathtoys',
+  '<a href="http://github.com/crisperdue/mathtoys"',
   ' target=_blank>GitHub project</a>',
   '<a href="http://prooftoys.org/">Prooftoys website</a>',
   '</ul>'
